7c897d009f8bb1cc69745c3c6d3debadee529f1b
alinarogova123-arch <alinarogova123@gmail.com>
2025-12-29 22:27:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,17 @@
-password = input('Введите пароль: ')
-
-password_list = list(password)
-
-
 def has_digit(password):
-    return any(symbol.isdigit() for symbol in password_list)
+    return any(symbol.isdigit() for symbol in password)
 
 
 def has_letters(password):
-    return any(symbol.isalpha() for symbol in password_list)
+    return any(symbol.isalpha() for symbol in password)
 
 
 def has_upper_letters(password):
-    return any(symbol.isupper() for symbol in password_list)
+    return any(symbol.isupper() for symbol in password)
 
 
 def has_lower_letters(password):
-    return any(symbol.islower() for symbol in password_list)
+    return any(symbol.islower() for symbol in password)
 
 
 def is_very_long(password):
@@ -24,10 +19,11 @@ def is_very_long(password):
 
 
 def has_symbols(password):
-    return any(len(password) > 0 and not symbol.isalpha() and not symbol.isdigit() for symbol in password_list)
+    return any(len(password) > 0 and not symbol.isalpha() and not symbol.isdigit() for symbol in password)
 
 
 def main():
+    password = input('Введите пароль: ')
     password_raiting = [
         has_digit(password),
         has_letters(password),
@@ -36,17 +32,15 @@ def main():
         has_upper_letters(password),
         has_symbols(password)
     ]
-
     score = 0
-
     for points in password_raiting:
         if points:
             score = score + 2
-
     print(score)
 
 
 if __name__ == '__main__':
     main()
+
 
 
